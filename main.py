ef1972a0f6bbac9eee416c925f822ddcbb54e362
Rinaldo <nikilson30@gmail.com>
2022-08-16 15:32:50 +0530

    Releasing version 1.0.0.1

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 import os
 import re
 import subprocess 
-import threading
 from wox import Wox
-from dependencies import clear_clipboard
-from dependencies import show_text
 
 class UnixPassMenu(Wox):
     window_prc = None
@@ -13,17 +10,22 @@ class UnixPassMenu(Wox):
     # query is default function to receive realtime keystrokes from wox launcher
     def query(self, query):
         results = []
+
+        # Listing the password files
         usr = os.path.expanduser('~')
         pass_dir = os.path.join(usr, '.password-store')
         password_files = [passwd_file for passwd_file in os.listdir(pass_dir) if not passwd_file.startswith('.')]
-        # password_files = ["hi", "bye", "hellow"]
+        
+        # Filtering the passwords based on the query
         rexp = re.compile(query)
         password_files = list(filter(rexp.match, password_files))
+
+        # Adding the passwords list in the wox 
         for passwd in password_files:
             results.append({
                 "Title": "{}".format(passwd[:-4]),
                 "SubTitle": "Key Word: {}".format(query),
-                # "IcoPath":"Images/app.png",
+                "IcoPath":"Images/key.png",
                 "ContextData": "ctxData",
                 "JsonRPCAction": {
                     'method': 'take_action',
@@ -39,32 +41,43 @@ class UnixPassMenu(Wox):
         results.append({
             "Title": "Context menu entry",
             "SubTitle": "Data: {}".format("Show Full Message"),
-            "IcoPath":"Images/app.png"
+            "IcoPath":"Images/key.png"
         })
         return results
 
     def take_action(self, filename, pass_dir):
         # Choose what to trigger on pressing enter on the result.
         # use SomeArgument to do something with data sent by parameters.
+
+        ## Hiding the console
         si = subprocess.STARTUPINFO()
         si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+        ## Reading the cipher file
         filename = os.path.join(pass_dir, filename)
         file = open(filename, "rb")
         cipher = file.read()
         file.close()
+
+        ## Reading the key id
         keyid_file = os.path.join(pass_dir, ".gpg-id")
         idfile = open(keyid_file, "r")
         keyid = idfile.read()
         idfile.close()
+
+        ## Decryting the file
         output = subprocess.check_output(["gpg", "-u", keyid, "-d", "{}".format(filename)], startupinfo=si).decode()
+        
+        ## Copying the password into the clipboard
         subprocess.run("clip.exe", universal_newlines=True, input=output[:output.find("\n")], startupinfo=si)
-        # window_prc = multiprocessing.Process(target = show_text, args = (output[output.find("\n")+1:],))
+        
+        ## Showing Other Credentials
         subprocess.Popen(["CommandlineTextBox.exe", output[output.find("\n")+1:]])
+        
+        ## Clearing the clipboard
         DETACHED_PROCESS = 0x00000008
         subprocess.Popen(["CleanClipboard.exe", "arg"], close_fds=True, creationflags=DETACHED_PROCESS)
-        # clipboard_prc = threading.Thread(target = clear_clipboard)
-        # clipboard_prc.setDaemon(True)
-        # clipboard_prc.start()
+
         return None
 
 if __name__ == "__main__":
